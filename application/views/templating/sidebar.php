95d34c2628a6a8a92be747588b187175ea1ebef0
haxan100 <abdul.hasan388@gmail.com>
2020-12-19 07:29:53 +0700

    perbaikan sidebar

--- application/views/templating/sidebar.php
+++ application/views/templating/sidebar.php
@@ -4,9 +4,9 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 	<!-- Brand Logo -->
-	<a href="index3.html" class="brand-link">
+	<a href="<?= base_url() ?>User/" class="brand-link">
 		<img src="<?= base_url(); ?>/aseets/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-		<span class="brand-text font-weight-light">AdminLTE 3</span>
+		<span class="brand-text font-weight-light">Pemilihan</span>
 	</a>
 
 	<!-- Sidebar -->
@@ -17,7 +17,7 @@
 				<img src="<?= base_url(); ?>/aseets/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
 			</div>
 			<div class="info">
-				<a href="#" class="d-block"><?= $data->nama ?> </a>
+				<a href="<?= base_url() ?>User/Profile" class="d-block"><?= $data->nama ?> </a>
 			</div>
 		</div>
 
@@ -36,7 +36,7 @@
 					</a>
 					<ul class="nav nav-treeview">
 						<li class="nav-item">
-							<a href="<?= base_url() ?>User/Profile" class="nav-link active">
+							<a href="<?= base_url() ?>User/Profile" class="nav-link ">
 								<i class="far fa-circle nav-icon"></i>
 								<p>Profile</p>
 							</a>
@@ -45,7 +45,7 @@
 
 					<ul class="nav nav-treeview">
 						<li class="nav-item">
-							<a href="<?= base_url() ?>admin/siswa" class="nav-link active">
+							<a href="<?= base_url() ?>admin/siswa" class="nav-link ">
 								<i class="far fa-circle nav-icon"></i>
 								<p>Master Siswa</p>
 							</a>
